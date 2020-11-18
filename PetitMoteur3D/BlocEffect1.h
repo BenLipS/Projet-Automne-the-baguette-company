50d62fa3121c3289680c1c597f060ead286c09b3
Drendal <draik.dral@gmail.com>
2020-11-18 20:40:15 +0100

    Initial files
    Rush 2 du prof

---
+++ PetitMoteur3D/BlocEffect1.h
@@ -0,0 +1,50 @@
+#pragma once
+#pragma once
+#include "objet3D.h"
+#include "Bloc.h"
+#include "d3dx11effect.h"
+
+namespace PM3D
+{
+
+	class CDispositifD3D11;
+
+	//  Classe : CBlocEffect1
+	//
+	//  BUT : 	Classe de bloc
+	//
+	class CBlocEffect1 : public CObjet3D
+	{
+	public:
+		CBlocEffect1(const float dx, const float dy, const float dz,
+			CDispositifD3D11* pDispositif);
+
+		// Destructeur
+		virtual ~CBlocEffect1();
+
+		virtual void Anime(float tempsEcoule) override;
+		virtual void Draw() override;
+
+	private:
+		CDispositifD3D11* pDispositif;
+		void InitShaders();
+		void InitEffect();
+
+		ID3DX11Effect* pEffet;
+		ID3DX11EffectTechnique* pTechnique;
+		ID3DX11EffectPass* pPasse;
+
+		ID3D11Buffer* pVertexBuffer;
+		ID3D11Buffer* pIndexBuffer;
+
+		ID3D11VertexShader* pVertexShader;
+		ID3D11PixelShader* pPixelShader;
+		ID3D11InputLayout* pVertexLayout;
+
+		// Définitions des valeurs d'animation
+		ID3D11Buffer* pConstantBuffer;
+		XMMATRIX matWorld;
+		float rotation;
+	};
+
+} // namespace PM3D
---
+++ PetitMoteur3D/objet3d.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "dispositif.h"
+#include <string>
+
+using namespace DirectX;
+
+namespace PM3D
+{
+//  Classe : CObjet3D
+//
+//  BUT : 	Classe de base de tous nos objets 3D
+//
+class CObjet3D
+{
+public:
+	virtual ~CObjet3D() = default;
+
+	virtual void Anime(float) {};
+	virtual void Draw() = 0;
+
+	std::string typeTag;
+};
+
+} // namespace PM3D
